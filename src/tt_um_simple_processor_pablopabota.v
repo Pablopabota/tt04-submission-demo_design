15acab754da7be26dbc4d1b7ae98053cd10d60c2
Pablopabota <pablis_94@hotmail.com>
2023-09-06 12:01:25 -0300

    Update tt_um_simple_processor_pablopabota.v

--- src/tt_um_simple_processor_pablopabota.v
+++ src/tt_um_simple_processor_pablopabota.v
@@ -1,6 +1,5 @@
 `default_nettype none
 //`include "simple_8b_alu.v"
-`include "cells.v"
 
 module tt_um_simple_processor_pablopabota #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
