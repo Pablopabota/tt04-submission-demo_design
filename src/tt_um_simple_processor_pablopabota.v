5c70980453d93d14e2ed88fdf347a92672243503
Pablopabota <pablis_94@hotmail.com>
2023-09-07 02:47:08 -0300

    uo_out a GND

--- src/tt_um_simple_processor_pablopabota.v
+++ src/tt_um_simple_processor_pablopabota.v
@@ -15,6 +15,7 @@ module tt_um_simple_processor_pablopabota #( parameter MAX_COUNT = 24'd10_000_00
     assign uio_oe = 8'b00000000;
     // unused bidirectional have to be tied to GND
     assign uio_out = 0;
+    assign uo_out = 0;
 
     // Simple mux for SDA line
     wire sda_wire;
